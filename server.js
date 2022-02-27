7135f83e94aa6b87098fd5f2e798c0a3d151cb8c
Zagitalc <zachary.13281@gmail.com>
2022-02-27 10:09:17 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,25 +5,68 @@ app.use(express.static(`client`));
 
 let articles = {
     "Title": "Why are we vaccinating children against COVID-19?",
-    "AuthorID": 1,
+    "Author": [
+        {
+        "Name":"",
+        "AuthorID":""
+        }
+    ],
+    "publisher":"",
+    "publishinfo":[
+
+        {
+            "Volume":"8",
+            "Issue no":"n/a"
+        }
+    ],
     "Date": "2021",
     
-    "Volume":"8",
-    "Issue no":"",
+    
+    
     "Image": "",
+    "Highlights":"",
     "Abstract": "blahblahblah</p>",
     "Comments": [
         {
-            "Name": "abc",
+            "Name": "abcd12",
             "Text": ""
         },
         
         {
-            "Name": "YouMadBroda",
+            "Name": "plxy99",
             "Text": ""
         }
     ]
 }
+
+let reftype={
+    "Harvard":"",
+    "Vancouver":"",
+    "IEEE":""
+
+}
+
+let referencedata = {
+    "reference number":"[9]",
+    "Initials":[{
+
+    
+        "Initial":"",
+        "Surname":""
+
+    }
+    
+
+    ],
+    "Type of publication":"",
+    "Date of publication":"",
+    "DOI":"",
+    "URL":""
+
+
+}
+
+
 app.get('/list', function (req, resp){
     resp.send(instruments);
 });
