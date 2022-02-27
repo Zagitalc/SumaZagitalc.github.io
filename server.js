35740cdf47c64f5315cea2cbc2c059d0c072cb95
Zagitalc <zachary.13281@gmail.com>
2022-02-27 09:42:39 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,27 @@ const app = express();
 
 app.use(express.static(`client`));
 
-//let instruments = [ 'piano', 'concertina', 'double bass'];
-
+let articles = {
+    "Title": "Why are we vaccinating children against COVID-19?",
+    "AuthorID": 1,
+    "Date": "2021",
+    
+    "Volume":"8",
+    "Issue no":"",
+    "Image": "",
+    "Abstract": "blahblahblah</p>",
+    "Comments": [
+        {
+            "Name": "abc",
+            "Text": ""
+        },
+        
+        {
+            "Name": "YouMadBroda",
+            "Text": ""
+        }
+    ]
+}
 app.get('/list', function (req, resp){
     resp.send(instruments);
 });
