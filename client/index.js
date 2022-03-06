b95364aefb0edbdce534aff553cfbee2c931342e
Zagitalc <zachary.13281@gmail.com>
2022-03-06 23:04:03 +0000

    1

--- client/index.js
+++ client/index.js
@@ -1,25 +1,110 @@
-window.addEventListener('click', async function(event){
-    try{
-      let response = await fetch('http://127.0.0.1:8090/searchpoint');
-      let body = await response.text();
-      document.getElementById('content').innerHTML=body;
-    } catch(e) {
-      this.alert(e);
-      //alert('hjwdbhbwdjhxbhdwbxw congrats error');
-    }
+const main = document.getElementById("myhome")
+const divContainer = document.getElementById("articlecontainer");
+
+function loadmain() {
+  main.style.display = "block"
+  divContainer.style.display = "none"
+
+
+}
+const homebtn = document.getElementById("homebtn")
+homebtn.addEventListener("button", async (event) => {
+
+  loadmain()
 });
+
+
+//search and load related articles
+async function loadJson(params) {
+  const response = await fetch('http://127.0.0.1:8090/searchpoint?' + params);
+  //method :'GET'
+  console.log("got respose");
+  const data = await response.json();
+  return data;
+
+}
+
+const sf = document.getElementById("searchform")
+sf.addEventListener("submit", async (event) => {
+
+  event.preventDefault()
+  const data = new FormData(sf)
+  const params = new URLSearchParams(data)
+
+  try {
+    const articles = await loadJson(params);
+
+    //const divContainer = document.getElementById("articlecontainer");
+    const authorlist = document.getElementById("authorcontainer")
+
+
+    divContainer.innerHTML = ""
+
+
+
+    articles.forEach(article => {
+      //const paragraphElem = document.createElement('p');
+      //paragraphElem.innerText =
+      // article.authors.forEach(author => {
+      //   authorlist.innerHTML +=
+      //     `authors:${author.name}`
+
+
+      // });
+      divContainer.innerHTML +=
+        `<hr class="featurette-divider">
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">  ${article.title} . <span class="text-muted">It’ll blow your
+              mind.</span></h2>
+              
+          <p class="lead">${article.highlights} ${article.authors[0]}</p>
+          <p class="lead">DOI:<a class="lead" href="${article.doi}"> ${article.doi}</a></p>
+        </div>
+        <div class="col-md-5">
+          <img  class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
+          height="500"
+             src="${article.image}"  aria-label="Placeholder: 500x500"
+            preserveAspectRatio="xMidYMid slice" focusable="false">
+            
+            <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em"></text>
+          </img>
+
+        </div>
+
+
+
+
+
+
+      title: ${article.title} 
+      
+      \n Date: ${article.date} \n `;
+      //divContainer.appendChild(paragraphElem);
+
+
+    });
+    divContainer.style.display = "block"
+
+  } catch (e) {
+    this.alert(e);
+    console.log(e);
+    alert('hjwdbhbwdjhxbhdwbxw congrats error');
+  }
+});
+
 
 // try to fetch get element by id
 //return the html content of the related articles
-window.addEventListener('load', async function(event){
-    try{
-      let response = await fetch('http://127.0.0.1:8090/new');
-      let body = await response.text();
-      document.getElementById('content').innerHTML=body;
-    } catch(e) {
-      this.alert(e);
-      //alert('hjwdbhbwdjhxbhdwbxw congrats error');
-    }
+window.addEventListener('later', async function (event) {
+  try {
+    let response = await fetch('http://127.0.0.1:8090/new');
+    let body = await response.text();
+    document.getElementById('content').innerHTML = body;
+  } catch (e) {
+    this.alert(e);
+    //alert('hjwdbhbwdjhxbhdwbxw congrats error');
+  }
 });
 
 
--- server.js
+++ server.js
@@ -3,45 +3,45 @@ const app = express();
 
 app.use(express.static(`client`));
 
-var bodyParser = require('body-parser');const res = require('express/lib/response');
- app.use(bodyParser.urlencoded({ extended: false }));
+var bodyParser = require('body-parser'); const res = require('express/lib/response');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 let articles = [
     {
-        "id":1,
-        "title": "Why are we vaccinating children against COVID-19?",
-        "author": [
+        "id": 1,
+        "title": "Why are we vaccinating children against COVID-19 a?",
+        "authors": [
             {
-                "name":"Ronald N.Kostoff",
-                "authorID":""
+                "name": "Ronald N.Kostoff",
+                "authorID": ""
             },
             {
-                "name":"Daniel Calina",
-                "authorID":""
-                }
+                "name": "Daniel Calina",
+                "authorID": ""
+            }
         ],
-        "publisher":"",
-        "publishinfo":[
+        "publisher": "",
+        "publishinfo": [
 
             {
-                "Volume":"8",
-                "Issue no":"n/a"
+                "Volume": "8",
+                "Issue no": "n/a"
             }
         ],
         "date": "2021",
-        "doi":"https://doi.org/10.1016/j.toxrep.2021.08.010",
-        
-        
+        "doi": "https://doi.org/10.1016/j.toxrep.2021.08.010",
+
+
         "image": "/pictures/covidarticle.jpg",
-        "highlights":"<p>Bulk of COVID-19 per capita deaths occur in elderly with high comorbidities <Br> <Br>Per capita COVID-19 deaths are negligible in children.</p>",
+        "highlights": "<p>Bulk of COVID-19 per capita deaths occur in elderly with high comorbidities <Br> <Br>Per capita COVID-19 deaths are negligible in children.</p>",
         "abstract": "blahblahblah</p>",
         "comments": [
             {
                 "name": "abcd12",
                 "text": "das ist ganz gut"
             },
-            
+
             {
                 "Name": "plxy99",
                 "Text": "doch!"
@@ -49,50 +49,50 @@ let articles = [
         ]
     },
     {
-        "id":2,
-        "title": "Total coloring of recursive maximal planar graphs",
-        "author": [
+        "id": 2,
+        "title": "Total coloring of recursive maximal planar graphs a",
+        "authors": [
             {
-            "name":"YangyangZhou",
-            "authorID":1
+                "name": "YangyangZhou",
+                "authorID": 1
             },
             {
-                "name":"DongyangZhao",
-                "authorID":2
+                "name": "DongyangZhao",
+                "authorID": 2
             },
             {
-                "name":"MingyuanMa",
-                "authorID":3
+                "name": "MingyuanMa",
+                "authorID": 3
             },
             {
-                "name":"JinXu",
-                "authorID":4
+                "name": "JinXu",
+                "authorID": 4
             }
-            
-            
-               
+
+
+
         ],
-        "publisher":"",
-        "publishinfo":[
+        "publisher": "",
+        "publishinfo": [
 
             {
-                "Volume":"8",
-                "Issue no":"n/a"
+                "Volume": "8",
+                "Issue no": "n/a"
             }
         ],
         "date": "2021",
-        "doi":"https://doi.org/10.1016/j.tcs.2022.01.024",
-        
-        
+        "doi": "https://doi.org/10.1016/j.tcs.2022.01.024",
+
+
         "image": "/pictures/A-3-connected-maximal-planar-graph-with-d-5-First-suppose-that-d-T-5-In-this-case.png",
-        "highlights":"<p>Bulk of COVID-19 per capita deaths occur in elderly with high comorbidities <Br> <Br>Per capita COVID-19 deaths are negligible in children.</p>",
+        "highlights": "<p>Bulk of COVID-19 per capita deaths occur in elderly with high comorbidities <Br> <Br>Per capita COVID-19 deaths are negligible in children.</p>",
         "abstract": "blahblahblah</p>",
         "comments": [
             {
                 "name": "abcd12",
                 "text": "das ist ganz gut"
             },
-            
+
             {
                 "name": "plxy99",
                 "text": "doch!"
@@ -100,50 +100,102 @@ let articles = [
         ]
     },
     {
-        "id":3,
+        "id": 3,
         "title": "A Real Time Processing system for big data in astronomy: Applications to HERA",
-        "author": [
+        "authors": [
             {
-                "name":"Ronald N.Kostoff",
-                "authorID":""
+                "name": "P.La Plante ",
+                "authorID": ""
             },
             {
-                "name":"Daniel Calina",
-                "authorID":""
-                }
+                "name": "P.K.G.Williams",
+                "authorID": ""
+            },
+            {
+                "name": "M.Kolopanis",
+                "authorID": ""
+            }
         ],
-        "publisher":"",
-        "publishinfo":[
+        "publisher": "",
+        "publishinfo": [
 
             {
-                "Volume":"8",
-                "Issue no":"n/a"
+                "Volume": "8",
+                "Issue no": "n/a"
             }
         ],
         "date": "2021",
-        "doi":"https://doi.org/10.1016/j.ascom.2021.100489",
-        
-        
+        "doi": "https://doi.org/10.1016/j.ascom.2021.100489",
+
+
         "image": "/pictures/escape-paper-astornomy.jpg",
-        "highlights":"<p>MethodsData analysis — Physical sciences and engineeringAstronomy — SoftwareData analysis — SoftwareDevelopment</p>",
+        "highlights": "MethodsData analysis — Physical sciences and engineeringAstronomy — SoftwareData analysis — SoftwareDevelopment</p>",
         "abstract": "<p>As current- and next-generation astronomical instruments come online, they will generate an unprecedented deluge of data. Analyzing these data in real time presents unique conceptual and computational challenges, and their long-term storage and archiving is scientifically essential for generating reliable, reproducible results. We present here the real-time processing (RTP) system for the Hydrogen Epoch of Reionization Array (HERA), a radio interferometer endeavoring to provide the first detection of the highly redshifted 21 cm signal from Cosmic Dawn and the Epoch of Reionization by an interferometer. The RTP system consists of analysis routines run on raw data shortly after they are acquired, such as calibration and detection of radio-frequency interference (RFI) events. RTP works closely with the Librarian, the HERA data storage and transfer manager which automatically ingests data and transfers copies to other clusters for post-processing analysis. Both the RTP system and the Librarian are public and open source software, which allows for them to be modified for use in other scientific collaborations. When fully constructed, HERA is projected to generate over 50 terabytes (TB) of data each night, and the RTP system enables the successful scientific analysis of these data.</p>",
         "comments": [
             {
                 "name": "abcd12",
                 "text": "das ist ganz gut"
             },
-            
+
             {
                 "Name": "plxy99",
                 "Text": "doch!"
             }
         ]
     },
+    {
+        "id": 4,
+        "title": "Identifying the influence of disaster education on the risk perception of rural residents in geohazard-prone areas: A propensity score-matched study",
+        "authors": [
+            {
+                "name": "Xiaohui Wang",
+                "authorID": ""
+            },
+            {
+                "name": "Li Peng  Wei Dengab",
+                "authorID": ""
+            },
+            {
+                "name": "Kexin Huang ",
+                "authorID": ""
+            },
+            {
+                "name": " Wei Dengab",
+                "authorID": ""
+            }
+        ],
+        "publisher": "",
+        "publishinfo": [
+
+            {
+                "Volume": "71",
+                "Issue no": "n/a"
+            }
+        ],
+        "date": "March 2021",
+        "doi": "https://doi.org/10.1016/j.ijdrr.2022.102795",
+
+
+        "image": "/pictures/riskreduction.jpg",
+        "highlights": `"Focus on informal education of local residents rather than formal education in schools. PSM is used to eliminate the bias in the estimation results caused by sample selection Different dimensions of risk perception are considered.Policy suggestions on disaster education methods were put forward to guide scientific risk perception."`,
+        "abstract": "blahblahblah</p>",
+        "comments": [
+            {
+                "name": "abcd12",
+                "text": "das ist ganz gut"
+            },
+
+            {
+                "Name": "plxy99",
+                "Text": "doch!"
+            }
+        ]
+    }
 
 ]
 
-let reftype=["Harvard","Vancouver","IEEE"]
-    
+let reftype = ["Harvard", "Vancouver", "IEEE"]
+
 let references = []
 
 
@@ -151,49 +203,40 @@ let references = []
 
 
 
-app.get('/articles', function (req, resp){
+app.get('/articles', function (req, resp) {
     resp.json(articles);
 
 });
 
 
+
 //search for title by keyword, return the suitabble title
-app.get('/searchpoint',function(req,resp){
+app.get('/searchpoint', function (req, res) {
     const search = req.query.search;
     let results = [];
-    //let keyword = []
-  
-    for (let i = 0; i < articles.length;i++){
-  
-      let article = articles[i];
-      if (article.title.includes(search)){
-        //insert at the last postion of the array
-        results.push(article.title);
-      } 
-    //   else {
-    //     resp.send("no match, please try again later!")
 
-    //   }
-      
-  
+    if (search == '') {
+        res.send(results)
+        return
+
     }
-    resp.format({
-        "text.html":()=>{
-            resp.send(`
-                <h1 class="example" ></h1> 
-                <h2 >The required Doi of  ${search} is ${results} </h2>  `);
-            
-            
+    for (let i = 0; i < articles.length; i++) {
+
+        let article = articles[i];
+        if (article.title.includes(search)) {
+            //insert at the last postion of the array
+            results.push(article);
         }
-        
-    })
-    //resp.send(results);
-  });
+
+    };
+    res.json(results)
+
+});
 
 //create your own citation using app.post
-app.post('/new', function (req,resp){
+app.post('/new', function (req, resp) {
 
-    
+
 
     console.log("got request");
     console.log(req.body);
@@ -201,37 +244,37 @@ app.post('/new', function (req,resp){
     const ref_no = req.body.ref_no;
     const initial = req.body.initial;
     const surname = req.body.surname;
-    const author ={
-    
+    const author = {
 
-    
-        "initial":initial,
-        "surname":surname
+
+
+        "initial": initial,
+        "surname": surname
 
 
     };
-    const initials =[];
+    const initials = [];
     initials.push(author);
     const title = req.body.title;
 
 
     const referencedata = {
-    "reference number": ref_no,
-    "initials":[{
+        "reference number": ref_no,
+        "initials": [{
 
-    
-        "initial":initials,
-        "surname":surname
 
-    }
-    
+            "initial": initials,
+            "surname": surname
 
-    ],
-    "title":title,
-    // "type of publication":pub_type,
-    // "date of publication":pub_date,
-    // "doi":doi_href,
-    // "url":href
+        }
+
+
+        ],
+        "title": title,
+        // "type of publication":pub_type,
+        // "date of publication":pub_date,
+        // "doi":doi_href,
+        // "url":href
 
 
     }
@@ -241,8 +284,8 @@ app.post('/new', function (req,resp){
 
 
 
-  
-  
+
+
 
 
 });
